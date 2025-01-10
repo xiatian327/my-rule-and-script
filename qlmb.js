9a5d9d1b9dafd17bbecf1209300110ba65e6905d
xiatian327 <136238840+xiatian327@users.noreply.github.com>
2025-01-10 19:05:11 +0800

    更新 qlmb.js

--- qlmb.js
+++ qlmb.js
@@ -16,9 +16,9 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/qlmB.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/xiatian327/my-rule-and-script/refs/heads/main/qlmb.js
 
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/qlmB.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://https://raw.githubusercontent.com/xiatian327/my-rule-and-script/refs/heads/main/qlmb.js
 
 [mitm] 
 
