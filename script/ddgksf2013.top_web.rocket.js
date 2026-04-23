a174a6fbf0563eddaa98740fa59c9f2746bbf093
xiatian327 <136238840+xiatian327@users.noreply.github.com>
2026-04-24 05:35:15 +0800

    更新 ddgksf2013.top_web.rocket.js

--- script/ddgksf2013.top_web.rocket.js
+++ script/ddgksf2013.top_web.rocket.js
@@ -1,15 +1,14 @@
 let body = $response.body
-  // 1. 保留核心：强制解锁网页隐藏内容
+  // 1. 解锁隐藏内容
   .replace(/Lock\s*=\s*\d/g, 'Lock=3')
   
-  // 2. 视觉修改：把界面上的按钮文字改成 Stash
-  .replace(/<\/i>\s*(QuantumultX|Shadowrocket)/gi, '</i> Stash')
+  // 2. 视觉修改：把 QX、小火箭、Surge 字眼全换成 Stash
+  .replace(/<\/i>\s*(QuantumultX|Shadowrocket|Surge)/gi, '</i> Stash')
   
-  // 3. 协议替换：把小火箭的唤醒协议改成 Stash 的覆写导入协议
-  .replace(/shadowrocket:\/\/install\?module=/gi, 'stash://install-override?url=')
+  // 3. 协议通杀替换：把这三家的唤醒协议，全部替换成 Stash 的导入协议
+  .replace(/(quantumult-x:\/\/\/add-resource\?remote-resource=|shadowrocket:\/\/install\?module=|surge:\/\/\/install-module\?url=)/gi, 'stash://install-override?url=')
   
-  // 4. 目标替换：告诉 Script Hub，我们需要转换成 stash-override 格式，而不是小火箭格式
-  .replace(/target(%3D|=)shadowrocket-module/gi, 'target$1stash-override')
-  .replace(/target(%3D|=)surge-module/gi, 'target$1stash-override');
+  // 4. 转换目标替换：强制告诉 Script Hub 输出 stash-override 格式
+  .replace(/target(%3D|=)(shadowrocket-module|surge-module|qx-rewrite)/gi, 'target$1stash-override');
 
 $done({ body });
